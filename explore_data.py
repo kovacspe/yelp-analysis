efbd335f103245496bbd95a896ea021e91cc8b5a
kovacspe <pet.kovacs1@gmail.com>
2020-10-20 17:17:08 +0200

    Add exporing sentiment and classification classes

--- explore_data.py
+++ explore_data.py
@@ -3,6 +3,7 @@ import os
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 
 
 def load_business():
@@ -32,10 +33,24 @@ def load_reviews():
     print(f'Columns:{reviews.columns}')
 
 
+def explore_sentiment(sentiment_stats_file):
+    data = pd.read_csv(sentiment_stats_file)
+    # Filter out rare occurances
+    data = data[data['count'] > 1000]
+    data = data[data['std'] < 1]
+    # Sort by std
+    data.sort_values(by=['mean'], ascending=True, inplace=True)
+    # Filter out neutral words
+    pos_data = data[data['mean'] > 4.25]
+    neg_data = data[data['mean'] < 1.75]
+    print(pos_data.head(30))
+    print(neg_data.head(30))
+
+
 def explore_reviews():
     # Load data
     numeric_data = read_numeric_data_from_reviews(
-        DATA_FILES['review'], max_lines=100000)
+        DATA_FILES['review'], max_lines=1000000)
     # clip useful attribute to interval 0-5
     numeric_data['useful_clipped'] = numeric_data.apply(
         lambda row: row['useful'] if row['useful'] < 30 else 30, axis=1)
@@ -77,6 +92,25 @@ def explore_reviews():
         f'Stars mean:{stars_mean}. Baseline MSE: {stars_mse}. Baseline crossentropy: {stars_scce}')
     print(numeric_data_agg)
 
+    # classify
+
+    def classify(num_stars):
+        if num_stars < 3:
+            return 1
+        elif num_stars == 3:
+            return 3
+        else:
+            return 5
+    numeric_data['class'] = numeric_data.apply(
+        lambda row: classify(row['stars']), axis=1)
+
+    counts, _ = np.histogram(numeric_data['class'], bins=[1, 3, 5, 5])
+    print(counts/len(numeric_data))
+    print(counts[[0, 2]]/np.sum(counts[[0, 2]]))
+    plt.title('Distribution of positive and negative reviews')
+    plt.bar(['neg', 'neut', 'pos'], height=counts/len(numeric_data))
+    plt.show()
+
     useful_only = numeric_data[numeric_data['useful'] >= 30]
     plt.title('Distribution of stars in useful reviews vs all reviews')
     plt.hist([useful_only['stars'], numeric_data['stars']],
@@ -97,4 +131,6 @@ def explore_reviews():
     plt.show()
 
 
+path = os.path.join(
+    'data', 'regex_tokens_without_stop_words', 'sentiment.csv')
 explore_reviews()
