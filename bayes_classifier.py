e6933f83428047657cd330e105dd903c225be95c
kovacspe <pet.kovacs1@gmail.com>
2020-10-22 22:17:29 +0200

    rebuild for testing all datasets

--- bayes_classifier.py
+++ bayes_classifier.py
@@ -33,26 +33,57 @@ class BayesClassifier:
 
 
 if __name__ == "__main__":
-    train_pos_data_path = os.path.join(
-        'data', 'regex_tokens_without_stop_words', 'pos_dict.csv')
-    train_neg_data_path = os.path.join(
-        'data', 'regex_tokens_without_stop_words', 'neg_dict.csv')
-    test_data_path = os.path.join(
-        'data', 'test_regex_tokens_without_stop_words', 'stemmed_data.csv')
-    classifier = BayesClassifier(
-        train_pos_data_path, train_neg_data_path, 0.73679818)
-    dataset = Dataset(os.path.join(test_data_path))
-    dataset.load_all()
+    # dataset paths
+    data_path = 'data'
+    train_data = os.path.join('data', 'regex_tokens')
+    test_data = os.path.join('data', 'test_regex_tokens')
+    train_data_stop_words = os.path.join(
+        'data', 'regex_tokens_without_stop_words')
+    test_data_stop_words = os.path.join(
+        'data', 'test_regex_tokens_without_stop_words')
+    for train, test in [(train_data, test_data), (train_data_stop_words, test_data_stop_words)]:
+        # stars
+        train_pos_data_path = os.path.join(train, 'pos_dict.csv')
+        train_neg_data_path = os.path.join(train, 'neg_dict.csv')
 
-    dataset.data = dataset.data[dataset.data['stars'] != 3]
-    dataset.data['gold'] = dataset.data.apply(
-        lambda row: row['stars'] > 3, axis=1)
-    dataset.data['predicted'] = dataset.data.apply(
-        lambda row: classifier.classify(row['text']), axis=1)
+        # useful
+        train_useful_data_path = os.path.join(train, 'useful_dict.csv')
+        train_useless_data_path = os.path.join(train, 'useless_dict.csv')
 
-    gold = dataset.data['gold']
-    predicted = dataset.data['predicted']
-    dataset.data.to_csv('predicted.csv')
-    print(f'Accuracy: {metrics.accuracy_score(gold,predicted)}')
-    print(f'F-score: {metrics.accuracy_score(gold,predicted)}')
-    print(f'Confusion matrix: {metrics.confusion_matrix(gold,predicted)}')
+        # test set
+        test_data_path = os.path.join(test, 'stemmed_data.csv')
+
+        stars_classifier = BayesClassifier(
+            train_pos_data_path, train_neg_data_path, 0.73679818)
+        useful_classifier = BayesClassifier(
+            train_useful_data_path, train_useless_data_path, 0.45751942)
+        test_dataset = Dataset(os.path.join(test_data_path))
+        test_dataset.load_all()
+
+        types = [
+            ('stars', 4, stars_classifier),
+            ('useful', 1, useful_classifier)
+        ]
+
+        for t, threshold, classifier in types:
+            if t == 'stars':
+                test_dataset.data = test_dataset.data[test_dataset.data[t] != 3]
+            test_dataset.data[f'{t}_gold'] = test_dataset.data.apply(
+                lambda row: row[t] >= threshold, axis=1)
+            test_dataset.data[f'{t}_predicted'] = test_dataset.data.apply(
+                lambda row: classifier.classify(row['text']), axis=1)
+
+            gold = test_dataset.data[f'{t}_gold']
+            predicted = test_dataset.data[f'{t}_predicted']
+            print('--------------------------------')
+            print(f'Predictor: {t}, Dataset:{test}')
+            print(f'Accuracy: {metrics.accuracy_score(gold,predicted)}')
+            print(
+                f'Precision: {metrics.average_precision_score(gold,predicted)}')
+            print(f'Recall: {metrics.recall_score(gold,predicted)}')
+            print(f'F-score: {metrics.f1_score(gold,predicted)}')
+            print(
+                f'Confusion matrix: {metrics.confusion_matrix(gold,predicted)}')
+            print('--------------------------------')
+            # save test
+        test_dataset.data.to_csv('predicted.csv')
